136af69a64da9ac49fc6767c0ffc19fe90d52dda
niharika sahu <121661609+nihu-13@users.noreply.github.com>
2023-01-07 23:15:48 +0530

    Update userInput.py

--- userInput.py
+++ userInput.py
@@ -1,46 +1,44 @@
 # 1. Write a python script to take your name as input from the user and then print it.
-# name=input("Enter Your Name : ")
-# print("Name : ",name)
+name=input("Enter Your Name : ")
+print("Name : ",name)
 # 2. Write a python script to take input from the user. Input must be a number.
-# number=int(input("Enter A Number : "))
-# print(number)
+number=int(input("Enter A Number : "))
+print(number)
 # 3. Write a python script which takes two numbers from the user, then calculate their sum
 # and display the result.
-# print("Enter A Two Number : ",end="")
-# a,b=int(input()),int(input())
-# result=a+b
-# print("Result = ",result)
+print("Enter A Two Number : ",end="")
+a,b=int(input()),int(input())
+result=a+b
+print("Result = ",result)
 # 4. Write a python script which takes the radius from the user and display area of a circle.
-# radius=eval(input("Enter Radius : "))
-# areaOfCircle=(3.14*radius**2)
-# print("Area Of circle : ",areaOfCircle)
+radius=eval(input("Enter Radius : "))
+areaOfCircle=(3.14*radius**2)
+print("Area Of circle : ",areaOfCircle)
 # 5. Write a python script to calculate the square of a number. Number is entered by the user.
-# number=int(input("Enter a Number : "))
-# print("Square of a number : ",number**2)
+number=int(input("Enter a Number : "))
+print("Square of a number : ",number**2)
 # 6. Write a python script to calculate the area of Triangle. Number is entered by the user.
-# # b = int(input("Input the base : "))
-# # h = int(input("Input the height : "))
-
-# # area = b*h/2
-
-# # print("area of Triangle = ", area)
+b = int(input("Input the base : "))
+h = int(input("Input the height : "))
+area = b*h/2
+print("area of Triangle = ", area)
 # 7. Write a python script to calculate average of three numbers, entered by the user
-# print("Enter A Number")
-# num1,num2,num3=int(input()),int(input()),int(input())
-# averaage=(num1+num2+num3)/3
-# print("average of three numbers : ",averaage)
+print("Enter A Number")
+num1,num2,num3=int(input()),int(input()),int(input())
+averaage=(num1+num2+num3)/3
+print("average of three numbers : ",averaage)
 # 8. Write a python script to calculate simple interest
-# p=int(input("Enter Principal amount : "))
-# r=int(input("Entre Annual interest rate : "))
-# n=int(input("Enter Term of loan, in years : "))
-# simple_interest=p*r*n
-# print("simple interest : ",simple_interest)
+p=int(input("Enter Principal amount : "))
+r=int(input("Entre Annual interest rate : "))
+n=int(input("Enter Term of loan, in years : "))
+simple_interest=p*r*n
+print("simple interest : ",simple_interest)
 # 9. Write a python script to calculate the volume of a cuboid.
-# l=int(input("Enter length : "))
-# b=int(input("Enter bregth : "))
-# h=int(input("Enter hieght : "))
-# Volume_of_a_cuboid =l*b*h
-# print("Volume of a cuboid = ",Volume_of_a_cuboid) 
+l=int(input("Enter length : "))
+b=int(input("Enter bregth : "))
+h=int(input("Enter hieght : "))
+Volume_of_a_cuboid =l*b*h
+print("Volume of a cuboid = ",Volume_of_a_cuboid) 
 # 10. Write a python script to calculate area of a rectangle
 l=int(input("Enter length : "))
 w=int(input("Enter width : "))
