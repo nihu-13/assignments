8b4bc440814e635073479cd067e6c2dd6ca1a83a
niharika sahu <121661609+nihu-13@users.noreply.github.com>
2023-01-09 13:41:15 +0530

    Update for_Loop_Range.py

--- for_Loop_Range.py
+++ for_Loop_Range.py
@@ -1,35 +1,35 @@
 # 1. Write a python script to print MySirG N times on the screen
-# for x in range(int(input("Enter Number N : "))):
-#     print("MySirG")
+ for x in range(int(input("Enter Number N : "))):
+     print("MySirG")
 # 2. Write a python script to print first N natural numbers
-# r=range(int(input("Enter Number N : ")))
-# for x in r:
-#     print(x+1)
+ r=range(int(input("Enter Number N : ")))
+ for x in r:
+     print(x+1)
 # 3. Write a python script to print first N natural numbers in reverse order
-# r=range(int(input("Enter Number N")),0,-1)
-# for e in r:
-#     print(e)
+ r=range(int(input("Enter Number N")),0,-1)
+ for e in r:
+     print(e)
 # 4. Write a python script to print first N odd natural numbers
-# for x in range(1,2*int(input("Enter Number N : ")),2):
-#     print(x)
+ for x in range(1,2*int(input("Enter Number N : ")),2):
+     print(x)
 # 5. Write a python script to print first N odd natural numbers in reverse order
-# for x in range(2*int(input("Enter Number N : "))-1,0,-2):
-#     print(x)
+ for x in range(2*int(input("Enter Number N : "))-1,0,-2):
+     print(x)
 # 6. Write a python script to print first N even natural numbers
-# n=int(input("Enter Number N : "))
-# for x in range(1,2*n+1):
-#     if x%2==0:
-#         print(x)
+ n=int(input("Enter Number N : "))
+ for x in range(1,2*n+1):
+     if x%2==0:
+         print(x)
 # 7. Write a python script to print first N even natural numbers in reverse order
-# for x in range(2*int(input("Enter Number N : ")),0,-2):
-#     print(x)
+ for x in range(2*int(input("Enter Number N : ")),0,-2):
+     print(x)
 # 8. Write a python script to print squares of first N natural numbers
-# for x in range(int(input("Enter Number N : "))):
-#     print((x+1)**2)
+ for x in range(int(input("Enter Number N : "))):
+     print((x+1)**2)
 # 9. Write a python script to print cubes of first N natural numbers
-# for x in range(int(input("Enter Number N : "))):
-#     print((x+1)**3)
+ for x in range(int(input("Enter Number N : "))):
+     print((x+1)**3)
 # 10. Write a python script to print first 10 multiples of Nq
-n=int(input("Enter Number N : "))
+=int(input("Enter Number N : "))
 for x in range(1,11):
     print(x*n)
